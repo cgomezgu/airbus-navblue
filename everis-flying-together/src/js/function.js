d37a457bf9c246817720e6547ad5269508801eaf
cgomezgu <44551916+cgomezgu@users.noreply.github.com>
2021-03-03 11:51:35 +0100

    feat: add title into all slides

--- everis-flying-together/src/js/function.js
+++ everis-flying-together/src/js/function.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'; 
-import imageOriginal from '../images/draggable-comparator/libellule_blueprint_web.jpg';
+import imageOriginal from '../images/draggable-comparator/blueprint-wing-web.png';
 
 $(function () {
   const $cont = $('.container');
@@ -53,6 +53,8 @@ $(function () {
               <h1>Everis Flying Together</h1>
               <h3>We are ready to raise your wings.</h3>
               <h3>We will guide you on this incredible flight.</h3>
+              <h3>Before going ahead with this journey, don’t forget to pick these 4 fundamentals. 
+              </h3>
             </div>  
             <div class="contact-area">
               <div class="contact contact-1">
@@ -123,7 +125,7 @@ $(function () {
                   </section>             
                 </main>
                 <nav>
-                  <a href="#" class="gmail">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M16 3v10c0 .567-.433 1-1 1h-1V4.925L8 9.233 2 4.925V14H1c-.567 0-1-.433-1-1V3c0-.283.108-.533.287-.712C.467 2.107.718 2 1 2h.333L8 6.833 14.667 2H15c.283 0 .533.108.713.288.179.179.287.429.287.712z" fill-rule="evenodd"/></svg>
                     </div>
@@ -133,7 +135,7 @@ $(function () {
                     </div>                
                     <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"> <g class="nc-icon-wrapper" fill="#444444"> <path d="M17.17 32.92l9.17-9.17-9.17-9.17L20 11.75l12 12-12 12z"></path> </g> </svg>
                   </a>
-                  <a href="#" class="facebook">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M15.117 0H.883C.395 0 0 .395 0 .883v14.234c0 .488.395.883.883.883h7.663V9.804H6.46V7.39h2.086V5.607c0-2.066 1.262-3.19 3.106-3.19.883 0 1.642.064 1.863.094v2.16h-1.28c-1 0-1.195.48-1.195 1.18v1.54h2.39l-.31 2.42h-2.08V16h4.077c.488 0 .883-.395.883-.883V.883C16 .395 15.605 0 15.117 0" fill-rule="nonzero"/></svg>
                     </div>
@@ -143,7 +145,7 @@ $(function () {
                     </div>                
                     <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"> <g class="nc-icon-wrapper" fill="#444444"> <path d="M17.17 32.92l9.17-9.17-9.17-9.17L20 11.75l12 12-12 12z"></path> </g> </svg>
                   </a>
-                  <a href="#" class="twitter">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.37-1.337.64-2.085.79-.598-.64-1.45-1.04-2.396-1.04-1.812 0-3.282 1.47-3.282 3.28 0 .26.03.51.085.75-2.728-.13-5.147-1.44-6.766-3.42C.83 2.58.67 3.14.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.41-.02-.61-.058.42 1.304 1.63 2.253 3.07 2.28-1.12.88-2.54 1.404-4.07 1.404-.26 0-.52-.015-.78-.045 1.46.93 3.18 1.474 5.04 1.474 6.04 0 9.34-5 9.34-9.33 0-.14 0-.28-.01-.42.64-.46 1.2-1.04 1.64-1.7z" fill-rule="nonzero"/></svg>
                     </div>
@@ -173,7 +175,7 @@ $(function () {
                   </section>             
                 </main>
                 <nav>
-                  <a href="#" class="gmail">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M16 3v10c0 .567-.433 1-1 1h-1V4.925L8 9.233 2 4.925V14H1c-.567 0-1-.433-1-1V3c0-.283.108-.533.287-.712C.467 2.107.718 2 1 2h.333L8 6.833 14.667 2H15c.283 0 .533.108.713.288.179.179.287.429.287.712z" fill-rule="evenodd"/></svg>
                     </div>
@@ -183,7 +185,7 @@ $(function () {
                     </div>                
                     <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"> <g class="nc-icon-wrapper" fill="#444444"> <path d="M17.17 32.92l9.17-9.17-9.17-9.17L20 11.75l12 12-12 12z"></path> </g> </svg>
                   </a>
-                  <a href="#" class="facebook">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M15.117 0H.883C.395 0 0 .395 0 .883v14.234c0 .488.395.883.883.883h7.663V9.804H6.46V7.39h2.086V5.607c0-2.066 1.262-3.19 3.106-3.19.883 0 1.642.064 1.863.094v2.16h-1.28c-1 0-1.195.48-1.195 1.18v1.54h2.39l-.31 2.42h-2.08V16h4.077c.488 0 .883-.395.883-.883V.883C16 .395 15.605 0 15.117 0" fill-rule="nonzero"/></svg>
                     </div>
@@ -193,7 +195,7 @@ $(function () {
                     </div>                
                     <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"> <g class="nc-icon-wrapper" fill="#444444"> <path d="M17.17 32.92l9.17-9.17-9.17-9.17L20 11.75l12 12-12 12z"></path> </g> </svg>
                   </a>
-                  <a href="#" class="twitter">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.37-1.337.64-2.085.79-.598-.64-1.45-1.04-2.396-1.04-1.812 0-3.282 1.47-3.282 3.28 0 .26.03.51.085.75-2.728-.13-5.147-1.44-6.766-3.42C.83 2.58.67 3.14.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.41-.02-.61-.058.42 1.304 1.63 2.253 3.07 2.28-1.12.88-2.54 1.404-4.07 1.404-.26 0-.52-.015-.78-.045 1.46.93 3.18 1.474 5.04 1.474 6.04 0 9.34-5 9.34-9.33 0-.14 0-.28-.01-.42.64-.46 1.2-1.04 1.64-1.7z" fill-rule="nonzero"/></svg>
                     </div>
@@ -223,7 +225,7 @@ $(function () {
                   </section>             
                 </main>
                 <nav>
-                  <a href="#" class="gmail">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M16 3v10c0 .567-.433 1-1 1h-1V4.925L8 9.233 2 4.925V14H1c-.567 0-1-.433-1-1V3c0-.283.108-.533.287-.712C.467 2.107.718 2 1 2h.333L8 6.833 14.667 2H15c.283 0 .533.108.713.288.179.179.287.429.287.712z" fill-rule="evenodd"/></svg>
                     </div>
@@ -233,7 +235,7 @@ $(function () {
                     </div>                
                     <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"> <g class="nc-icon-wrapper" fill="#444444"> <path d="M17.17 32.92l9.17-9.17-9.17-9.17L20 11.75l12 12-12 12z"></path> </g> </svg>
                   </a>
-                  <a href="#" class="facebook">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M15.117 0H.883C.395 0 0 .395 0 .883v14.234c0 .488.395.883.883.883h7.663V9.804H6.46V7.39h2.086V5.607c0-2.066 1.262-3.19 3.106-3.19.883 0 1.642.064 1.863.094v2.16h-1.28c-1 0-1.195.48-1.195 1.18v1.54h2.39l-.31 2.42h-2.08V16h4.077c.488 0 .883-.395.883-.883V.883C16 .395 15.605 0 15.117 0" fill-rule="nonzero"/></svg>
                     </div>
@@ -243,7 +245,7 @@ $(function () {
                     </div>                
                     <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"> <g class="nc-icon-wrapper" fill="#444444"> <path d="M17.17 32.92l9.17-9.17-9.17-9.17L20 11.75l12 12-12 12z"></path> </g> </svg>
                   </a>
-                  <a href="#" class="twitter">
+                  <a href="#">
                     <div class="icon">
                       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.37-1.337.64-2.085.79-.598-.64-1.45-1.04-2.396-1.04-1.812 0-3.282 1.47-3.282 3.28 0 .26.03.51.085.75-2.728-.13-5.147-1.44-6.766-3.42C.83 2.58.67 3.14.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.41-.02-.61-.058.42 1.304 1.63 2.253 3.07 2.28-1.12.88-2.54 1.404-4.07 1.404-.26 0-.52-.015-.78-.045 1.46.93 3.18 1.474 5.04 1.474 6.04 0 9.34-5 9.34-9.33 0-.14 0-.28-.01-.42.64-.46 1.2-1.04 1.64-1.7z" fill-rule="nonzero"/></svg>
                     </div>
@@ -255,9 +257,6 @@ $(function () {
                   </a>
                 </nav>
               </div>
-              
-                
-               
             </div> 
           </div>
           <div class="clouds">
@@ -277,32 +276,41 @@ $(function () {
         break;
       case 2 :
         $slide =
-          $(`<div data-target="${numSlide}" class="slide slide--${numSlide}">           
-              <div class="slide-container">
-                <div class="${slideComparatorContainer}">
-                  <figure class="image-container">
-                    <div class="image-label">
-                      <h3>Trend</h3>
-                      <ul>
-                        <li>texto 1</li>
-                        <li>texto 2</li>
-                        <li>texto 3</li>
-                        <li>texto 4</li>
-                      </ul>
-                    </div>
-                   
-                    <img src=${imageOriginal} alt="Original Image"> 
-                    <div class="resize-image"> 
-                    </div>              
-                    <span class="handle"></span>
-                  </figure> 
-                </div>
-              </div> 
+          $(`<div data-target="${numSlide}" class="slide slide--${numSlide}">    
+            <div class="slide__text-wrapper">
+              <h1>Strong Wings</h1>
+              <h3>Just like the strong wings of dragonflies, we adapt our products to your needs and get the best out of them.
+              </h3>
+            </div>                    
+            <div class="slide-container">
+              <div class="${slideComparatorContainer}">
+                <figure class="image-container">
+                  <div class="image-label">
+                    <h3>Trend</h3>
+                    <ul>
+                      <li>texto 1</li>
+                      <li>texto 2</li>
+                      <li>texto 3</li>
+                      <li>texto 4</li>
+                    </ul>
+                  </div>                   
+                  <img src=${imageOriginal} alt="Original Image"> 
+                  <div class="resize-image"> 
+                  </div>              
+                  <span class="handle"></span>
+                </figure> 
+              </div>
+            <div class="slide__darkbg slide--${numSlide}__darkbg slide--${numSlide}__animated-gradient"></div>
           </div>`);
         break;
         case 3:
           $slide =
             $(`<div data-target="${numSlide}" class="slide slide--${numSlide}">  
+              <div class="slide__text-wrapper">
+                <h1>Planned delivery</h1>
+                <h3>Like the versatility of a bird's wings, we make the delivery go as planned.
+                </h3>
+              </div>  
               <div class="clouds">
                 <img class="cloud-1" style="--i:1;"/>
                 <img class="cloud-2" style="--i:2;"/>
@@ -317,13 +325,18 @@ $(function () {
               </div>
               <div class="slide__darkbg slide--${numSlide}__darkbg slide--${numSlide}__animated-gradient"></div>            
               <div class="video-container">
-                <iframe poster="https://www.emailonacid.com/images/blog_images/Emailology/2013/html5_video/bunny_cover.jpg"  src="https://player.vimeo.com/video/518638996" width="640" height="480" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                <iframe poster="https://www.emailonacid.com/images/blog_images/Emailology/2013/html5_video/bunny_cover.jpg" src="https://player.vimeo.com/video/518638996" class="with-shadow" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
               </div>  
             </div>`);
         break;
         case 4:
           $slide =
-            $(`<div data-target="${numSlide}" class="slide slide--${numSlide}">     
+            $(`<div data-target="${numSlide}" class="slide slide--${numSlide}">   
+              <div class="slide__text-wrapper">
+                <h1>Everis Flying Together</h1>
+                <h3>We are ready to raise your wings.</h3>
+                <h3>We will guide you on this incredible flight.</h3>
+              </div>    
               <div class="clouds">
                 <img class="cloud-1" style="--i:1;"/>
                 <img class="cloud-2" style="--i:2;"/>
@@ -356,6 +369,12 @@ $(function () {
           $slide = 
             $(`<div data-target="${numSlide}" class="slide slide--${numSlide}">
               <div class="slide__darkbg slide--${numSlide}__darkbg"></div>
+              <div class="slide__text-wrapper">
+                <h1>Let´s take off together</h1>
+                <h3>We just consolidated an awesome trip together. But it is the beginning of the flight.
+                </h3>
+                <h3>Experience our different products/demos.</h3>
+              </div>  
               <div class="button-container">
                 <ul>
                   <li>
